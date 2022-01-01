2b192e78a4ca9cf08f8b3626721e867e24a25de5
sayyid <sayyidkhan92@hotmail.com>
2022-01-01 18:41:58 +0800

    first commit

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,144 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import FacebookProvider from "next-auth/providers/facebook"
+import GithubProvider from "next-auth/providers/github"
+import TwitterProvider from "next-auth/providers/twitter"
+import Auth0Provider from "next-auth/providers/auth0"
+import CredentialProvider from "next-auth/providers/credentials"
+// import AppleProvider from "next-auth/providers/apple"
+// import EmailProvider from "next-auth/providers/email"
+import bcrypt from 'bcrypt'
+
+
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+export default NextAuth({
+  // https://next-auth.js.org/configuration/providers
+  providers: [
+    CredentialProvider({
+      name: "credentials",
+      credentials: {
+        username: {
+          label: "email",
+          type: "text",
+          placeholder: "john",
+        },
+        password: { label: "Password", type: "password" }
+      },
+      authorize: (credential) => {
+        
+        const username = credential.username;
+        const password = credential.password;
+        // todo: database lookup
+        if (username === "john" && password === "test") {
+          return {
+            id: 2,
+            name: "john",
+            email: "johndoe@test.com"
+          };
+        }
+        // login failed
+        return null;
+      }
+    }),
+  ],
+  // The secret should be set to a reasonably long random string.
+  // It is used to sign cookies and to sign and encrypt JSON Web Tokens, unless
+  // a separate secret is defined explicitly for encrypting the JWT.
+  secret: process.env.SECRET,
+
+  session: {
+    // Use JSON Web Tokens for session instead of database sessions.
+    // This option can be used with or without a database for users/accounts.
+    // Note: `strategy` should be set to 'jwt' if no database is used.
+    strategy: "jwt",
+
+    // Seconds - How long until an idle session expires and is no longer valid.
+    // maxAge: 30 * 24 * 60 * 60, // 30 days
+
+    // Seconds - Throttle how frequently to write to database to extend a session.
+    // Use it to limit write operations. Set to 0 to always update the database.
+    // Note: This option is ignored if using JSON Web Tokens
+    // updateAge: 24 * 60 * 60, // 24 hours
+  },
+
+  // JSON Web tokens are only used for sessions if the `jwt: true` session
+  // option is set - or by default if no database is specified.
+  // https://next-auth.js.org/configuration/options#jwt
+  jwt: {
+    // You can define your own encode/decode functions for signing and encryption
+    // if you want to override the default behaviour.
+    // encode: async ({ secret, token, maxAge }) => {},
+    // decode: async ({ secret, token, maxAge }) => {},
+  },
+
+  // You can define custom pages to override the built-in ones. These will be regular Next.js pages
+  // so ensure that they are placed outside of the '/api' folder, e.g. signIn: '/auth/mycustom-signin'
+  // The routes shown here are the default URLs that will be used when a custom
+  // pages is not specified for that route.
+  // https://next-auth.js.org/configuration/pages
+  pages: {
+    // signIn: '/auth/signin',  // Displays signin buttons
+    // signOut: '/auth/signout', // Displays form with sign out button
+    // error: '/auth/error', // Error code passed in query string as ?error=
+    // verifyRequest: '/auth/verify-request', // Used for check email page
+    // newUser: null // If set, new users will be directed here on first sign in
+  },
+
+  // Callbacks are asynchronous functions you can use to control what happens
+  // when an action is performed.
+  // https://next-auth.js.org/configuration/callbacks
+  callbacks: {
+    jwt: ({ token, user }) => {
+      // first time jwt callback is run, user object is available
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session: ({ session, token }) => {
+      if (token) {
+        session.id = token.id;
+      }
+      return session;
+    },
+    // async signIn({ user, account, profile, email, credentials }) { return true },
+    // async redirect({ url, baseUrl }) { return baseUrl },
+    // async session({ session, token, user }) { return session },
+    // async jwt({ token, user, account, profile, isNewUser }) { return token }
+  },
+
+  // Events are useful for logging
+  // https://next-auth.js.org/configuration/events
+  events: {},
+
+  // You can set the theme to 'light', 'dark' or use 'auto' to default to the
+  // whatever prefers-color-scheme is set to in the browser. Default is 'auto'
+  theme: {
+    colorScheme: "light",
+  },
+
+  // Enable debug messages in the console if you are having problems
+  debug: true,
+})
+
+const loginUser = async ({user, password}) => {
+  if(!user.password) {
+    throw new Error("Accounts Have to login with password.");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if(!isMatch) {
+    throw new Error("Password Incorrect.");
+  }
+  
+  return user;
+};
+
+const registerUser = async({email, password}) => {
+  const saltRounds = 10;
+  const hashPass = await bcrypt.hash(password, saltRounds)
+  // save the record into the database
+  // const newUser = new Users({ email, password: hashPass })
+  // await newUser.save()
+}
