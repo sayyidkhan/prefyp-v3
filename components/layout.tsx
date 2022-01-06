a0f8a3594748da35d61d2844310613b467867c15
sayyid <sayyidkhan92@hotmail.com>
2022-01-07 00:26:33 +0800

    added the register page

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,7 @@
-import Header from './header'
-import Footer from './footer'
+import Header from './header';
+import Footer from './footer';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function Layout ({children}) {
   return (
